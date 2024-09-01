e6bda115d9d4a964e2932f24bbe29561f93f0494
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-09-01 14:53:18 +0300

    feat: dbconfig type

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,3 +1,10 @@
+import type { DataSourceOptions } from 'typeorm';
+
+export type DbConfig = DataSourceOptions & {
+  enableSslAuth: boolean;
+  sslPaths: { ca: string; cert: string; key: string };
+};
+
 export interface IConfig {
   get: <T>(setting: string) => T;
   has: (setting: string) => boolean;

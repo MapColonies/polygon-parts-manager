022b33f10752ff0aaee7b8b1d86d78ebcc68ab77
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-09-08 14:43:48 +0300

    refactor: set custom db resources naming strategy

--- dataSource.ts
+++ dataSource.ts
@@ -1,9 +1,17 @@
 import config from 'config';
-import { DataSource } from 'typeorm';
+import { DataSource, DefaultNamingStrategy, Table } from 'typeorm';
 import { ConnectionManager } from './src/common/connectionManager';
 import { DbConfig } from './src/common/interfaces';
 
 const connectionOptions = config.get<DbConfig>('db');
+
+const customNamingStrategy = new DefaultNamingStrategy();
+customNamingStrategy.indexName = (tableOrName: Table | string, columnNames: string[], where?: string): string => {
+  return `${typeof tableOrName === 'string' ? tableOrName : tableOrName.name}_${columnNames.join('_')}${where !== undefined ? '_partial' : ''}_idx`;
+};
+customNamingStrategy.uniqueConstraintName = (tableOrName: Table | string, columnNames: string[]): string => {
+  return `${typeof tableOrName === 'string' ? tableOrName : tableOrName.name}_${columnNames.join('_')}`;
+}
 
 export const appDataSource = new DataSource({
   ...ConnectionManager.createConnectionOptions(connectionOptions),
@@ -13,4 +21,5 @@ export const appDataSource = new DataSource({
   migrations: ['src/db/migrations/*.ts'],
   migrationsRun: false,
   migrationsTableName: 'polygon_parts_migrations',
+  namingStrategy: customNamingStrategy,
 });

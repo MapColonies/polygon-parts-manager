aaeca47378a047daa57cd1d8698efc165ae96997
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-09-24 09:29:30 +0300

    refactor: error constructor params

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,8 +1,9 @@
+import httpStatusCodes from 'http-status-codes';
 import { InternalServerError } from '@map-colonies/error-types';
 
 export class DBConnectionError extends InternalServerError {
-  public constructor() {
-    super('Internal Server Error');
+  public constructor(message?: string) {
+    super(message ?? httpStatusCodes.getStatusText(httpStatusCodes.INTERNAL_SERVER_ERROR));
     Object.setPrototypeOf(this, InternalServerError.prototype);
   }
 }

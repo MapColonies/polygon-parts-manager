d8d544a9787443e3225db7653cf50089c281f470
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-10-01 13:07:08 +0300

    refactor: typo in interface name

--- src/polygonParts/models/interfaces.ts
+++ src/polygonParts/models/interfaces.ts
@@ -1,7 +1,7 @@
 import type { IPolygonPart, PolygonPart, PolygonPartsPayload } from '@map-colonies/mc-model-types';
 
 interface CommonPayload extends Omit<PolygonPartsPayload, 'partsData'>, PolygonPart {}
-interface CommonPreoperties extends Readonly<Omit<CommonPayload, 'countries' | 'cities' | 'sensors'>> {
+interface CommonProperties extends Readonly<Omit<CommonPayload, 'countries' | 'cities' | 'sensors'>> {
   readonly countries?: string;
   readonly cities?: string;
   readonly sensors: string;
@@ -12,14 +12,14 @@ interface PolygonPartProperties extends Readonly<Pick<IPolygonPart, 'id' | 'part
 /**
  * Common record properties of part and polygon part
  */
-export interface CommonRecord extends Readonly<Omit<PolygonPartsPayload, 'partsData'>>, CommonPreoperties {
+export interface CommonRecord extends Readonly<Omit<PolygonPartsPayload, 'partsData'>>, CommonProperties {
   readonly ingestionDateUTC: Date;
 }
 
 /**
  * Part record properties of the raw ingested part
  */
-export interface PartRecord extends CommonPreoperties, PartProperties {
+export interface PartRecord extends CommonProperties, PartProperties {
   readonly insertionOrder: number;
   readonly isProcessedPart: boolean;
 }
@@ -27,13 +27,13 @@ export interface PartRecord extends CommonPreoperties, PartProperties {
 /**
  * Polygon part record properties of the processed parts
  */
-export interface PolygonPartRecord extends CommonPreoperties, PolygonPartProperties {
+export interface PolygonPartRecord extends CommonProperties, PolygonPartProperties {
   readonly insertionOrder: number;
 }
 
 /**
  * Ingestion properties of polygon parts for create and update operations on DB
  */
-export interface IngestionProperties extends Omit<CommonPreoperties, 'ingestionDateUTC'> {
+export interface IngestionProperties extends Omit<CommonProperties, 'ingestionDateUTC'> {
   readonly ingestionDateUTC: undefined;
 }

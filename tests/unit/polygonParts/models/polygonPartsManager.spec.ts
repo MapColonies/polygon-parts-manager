c8b6d3fda46dc8939197babcd61309545703baa6
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-11-10 12:13:31 +0200

    test: cleanup

--- tests/unit/polygonParts/models/polygonPartsManager.spec.ts
+++ tests/unit/polygonParts/models/polygonPartsManager.spec.ts
@@ -1,22 +1,4 @@
-import jsLogger from '@map-colonies/js-logger';
-import { container } from 'tsyringe';
-import { ConnectionManager } from '../../../../src/common/connectionManager';
-import { SERVICES } from '../../../../src/common/constants';
-import { PolygonPartsManager } from '../../../../src/polygonParts/models/polygonPartsManager';
-
-let connectionManager: ConnectionManager;
-let polygonPartsManager: PolygonPartsManager;
-
 describe('PolygonPartsManager', () => {
-  beforeAll(() => {
-    const logger = jsLogger({ enabled: false });
-    container.register(SERVICES.LOGGER, { useValue: logger });
-    container.register(SERVICES.CONFIG, { useValue: {} });
-
-    connectionManager = container.resolve(ConnectionManager);
-    polygonPartsManager = container.resolve(PolygonPartsManager);
-  });
-
   describe('#createPolygonParts', () => {
     it.todo('should create the resource');
   });

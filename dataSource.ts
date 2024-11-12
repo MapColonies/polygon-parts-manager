f90f39f9810a8f788bd2390c2f7b7d4db52dd816
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-11-12 15:34:44 +0200

    refactor: centralize configs in default.json and add override config for migrations

--- dataSource.ts
+++ dataSource.ts
@@ -6,18 +6,18 @@ import type { DbConfig } from './src/common/interfaces';
 const connectionOptions = config.get<DbConfig>('db');
 
 const defaultDataSourceOptions = {
-  entities: ['src/**/DAL/*.ts'],
-  logging: true,
-  synchronize: false,
-  migrations: ['src/db/migrations/*.ts'],
-  migrationsRun: false,
-  migrationsTableName: 'migrations',
   namingStrategy,
-} satisfies Partial<DataSourceOptions>;
+};
+
+const overridingDataSourceOptions = {
+  entities: ['src/**/DAL/*.ts'],
+  migrations: ['src/db/migrations/*.ts'],
+};
 
 const dataSourceOptions: DataSourceOptions = {
   ...defaultDataSourceOptions,
   ...ConnectionManager.createConnectionOptions(connectionOptions),
+  ...overridingDataSourceOptions
 };
 
 export const appDataSource = new DataSource(dataSourceOptions);

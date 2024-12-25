ed9b48ba9cca865289cca7cd163af6fd88ac6d12
vitaligi <54726763+vitaligi@users.noreply.github.com>
2024-12-25 09:53:16 +0200

    style: linting

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -18,7 +18,7 @@ export interface ApplicationConfig {
   aggregation: {
     fixGeometry: {
       enabled: boolean;
-      bufferSize: number
+      bufferSize: number;
     };
     maxDecimalDigits: number;
   };

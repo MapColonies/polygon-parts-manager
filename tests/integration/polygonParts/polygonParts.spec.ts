287c2e8138b7b987d906b61f6f314cba3c17abb2
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-04-22 10:30:17 +0300

    test: error message for invalid JSON in find endpoint as in nodejs ver 20

--- tests/integration/polygonParts/polygonParts.spec.ts
+++ tests/integration/polygonParts/polygonParts.spec.ts
@@ -5604,8 +5604,8 @@ describe('polygonParts', () => {
         expect.assertions(3);
       });
 
-      it('should return 400 status code if feature collection in req body is an invalid value - is not an object', async () => {
-        const expectedErrorMessage = { message: 'Unexpected token i in JSON at position 0' };
+      it('should return 400 status code if feature collection in req body is an invalid value - is not an object - qwerty', async () => {
+        const expectedErrorMessage = { message: `Unexpected token 'i', "#" is not valid JSON` };
 
         const response = await requestSender.findPolygonParts({
           params: { polygonPartsEntityName: 'valid_name_orthophoto' as EntityIdentifier },

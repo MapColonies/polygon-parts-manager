53bf36692e0ba69004f5581237b94b9f3344c375
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-04-22 10:51:48 +0300

    test: fix error message

--- tests/integration/polygonParts/polygonParts.spec.ts
+++ tests/integration/polygonParts/polygonParts.spec.ts
@@ -5604,7 +5604,7 @@ describe('polygonParts', () => {
         expect.assertions(3);
       });
 
-      it('should return 400 status code if feature collection in req body is an invalid value - is not an object - qwerty', async () => {
+      it('should return 400 status code if feature collection in req body is an invalid value - is not an object', async () => {
         const expectedErrorMessage = { message: `Unexpected token 'i', "#" is not valid JSON` };
 
         const response = await requestSender.findPolygonParts({

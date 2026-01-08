a088b73ba852b76edc3a2506a7913741e2d28be3
razbroc <razbroc@gmail.com>
2026-01-08 10:34:39 +0200

    feat: add insertion_order column to validation table

--- src/polygonParts/DAL/validationPart.ts
+++ src/polygonParts/DAL/validationPart.ts
@@ -1,13 +1,17 @@
-import { Check, Column, Entity, Index, PrimaryColumn } from 'typeorm';
+import { Check, Column, Entity, Index, PrimaryColumn, Unique } from 'typeorm';
 import { MultiPolygon, Polygon } from 'geojson';
 import type { ValidatePartRecord } from '../models/interfaces';
 import { BasePart } from './basePart';
 
 @Entity({ name: 'validation_parts' })
+@Unique(['insertionOrder'])
 @Check('imaging times', `"imaging_time_begin_utc" <= "imaging_time_end_utc"`)
-export class ValidatePart extends BasePart implements ValidatePartRecord {
+export class ValidatePart extends BasePart implements ValidatePartRecord {  
   @PrimaryColumn({ type: 'text' })
   public readonly id!: string;
+
+  @Column({ type: 'bigint', insert: false, update: false, generated: 'identity', generatedIdentity: 'ALWAYS' })
+  public readonly insertionOrder!: number;
 
   // Use a generic 'Geometry' spatialFeatureType so the column can hold Polygon or MultiPolygon
   @Column({ type: 'geometry', spatialFeatureType: 'Geometry', srid: 4326, precision: 20 })
